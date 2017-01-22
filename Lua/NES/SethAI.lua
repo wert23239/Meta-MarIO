d2167281147500a78866d2fe7ff924f0c4fe9a75
wert23239 <lambe168@msu.edu>
2017-01-22 02:52:32 -0500

    MultiThreaded Code startup

--- Lua/NES/SethAI.lua
+++ Lua/NES/SethAI.lua
@@ -1599,6 +1599,87 @@ function loadFile(filename)
 	pool.currentFrame = pool.currentFrame + 1
 end
 
+--[[
+loadFile: takes in a pool file and reads the contents
+--]]
+function loadFileMulti(filename)
+
+    local file = io.open(filename, "r")
+    --Create a new gene pool
+    Population=1
+	pool = newPool()
+
+	--Read the generation
+	pool.generation = 0
+
+	--Gather MaxFitness
+	pool.maxFitness = 0
+
+		--Read the file
+		--[[
+		1 Line: Generation
+		2 Line: Max Fitness
+		3 Line: Number of Species
+		4 Line: Top Fitness for Species
+		5 Line: How Stale the species is
+		6 Line: Number of Genomes
+		7 Line: Top Fitness for set of genomes
+		8 Line: TODO: Max Neuron
+		9-23 Line: Set up diffent rates
+		24 Line: Number of Genes
+		For each gene 25- Number of Genes
+		2*.1 Line: Into Gene
+		2*.2 Line: Out Gene
+		2*.3 Line: Weight Gene
+		2*.4 Line: Innovation Gene
+		2*.5 Line: Enabled Boolean
+		Into Gene: What inputs need to be on the screen to run this
+		Out Gene: What controller output to execute
+		Weight Gene: How much the gene mattters
+		Inovation: TODO:
+		Enabled Gene: Determine whether to use the gene
+		--]]
+        local numSpecies = 1
+        for s=1,numSpecies do
+		local species = newSpecies()
+		table.insert(pool.species, species)
+		species.topFitness = 0
+		species.staleness = 0
+		local numGenomes = 1
+		for g=1,numGenomes do
+			local genome = newGenome()
+			table.insert(species.genomes, genome)
+			genome.fitness = file:read("*number")
+			genome.maxneuron = file:read("*number")
+			local line = file:read("*line")
+			while line ~= "done" do
+				genome.mutationRates[line] = file:read("*number")
+				line = file:read("*line")
+			end
+			local numGenes = file:read("*number")
+			for n=1,numGenes do
+				local gene = newGene()
+				table.insert(genome.genes, gene)
+				local enabled
+				gene.into, gene.out, gene.weight, gene.innovation, enabled = file:read("*number", "*number", "*number", "*number", "*number")
+				if enabled == 0 then
+					gene.enabled = false
+				else
+					gene.enabled = true
+				end
+
+			end
+		end
+	end
+        file:close()
+
+	while fitnessAlreadyMeasured() do
+		nextGenome()
+	end
+	initializeRun()
+	pool.currentFrame = pool.currentFrame + 1
+end
+
  --[[
 loadPool: Button clicked to load a population
 --]]
@@ -1638,8 +1719,12 @@ if pool == nil then
 		writeMultiIntial("Initial")
 		Population=tonumber(forms.getthreadNum())
 		client.SetGameExtraPadding(pool.generation,0,0,0)
-	end
+	elseif tonumber(forms.getthreadNum())>0  then
+	loadFileMulti("Muation")
+	mutate(pool.species[0].genome[0])
+	else
 	initializePool()
+	end
 	
 end
 
@@ -1899,7 +1984,7 @@ while true do
 			end
 			--sort fitnesscheck
 			table.sort(fitnesscheck)
-			console.writeline(fitnesscheck[0])
+		
 			fitness=fitnesscheck[2]
 			--Extra bonus for completeting the level
 			if gameinfo.getromname() == "Super Mario World (USA)" and rightmost > 4816 then
@@ -1914,10 +1999,15 @@ while true do
 				fitness = -1
 			end
 
+			if(tonumber(forms.getthreadNum())>0) then
+				writeMultiGenome("Genome"..tonumber(forms.getthreadNum()))
+				client.SetGameExtraPadding(pool.generation,0,0,0)
+			end
 			--Set the current genomes fitness to the local fitness
 			if genome.fitness < fitness then
 				genome.fitness = fitness
 			end
+
 
 
 			--If newfitness record then
